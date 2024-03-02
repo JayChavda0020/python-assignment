c00912eb2f57b174f199513cea397533324f8c30
JayChavda0020 <jaychavdam0020@gmail.com>
2024-03-02 23:49:47 +0530

    29-2-24

--- Module=2/Problem_1.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number: "))
-
-if n > 0:
-    print("number is positive")
-elif n < 0:
-    print("number is negative")
-else:
-    print("number is zero:")
--- Module=2/problem_2.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter N: "))
-f = 1
-
-while n > 0:
-    f = f*n
-    n -= 1
-print("factorial is: ", f)
